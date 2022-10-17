f13604c9e91376e42e89956e1979259be2edf916
Shin-sangwon <dnjsml10@gmail.com>
2022-10-17 23:20:06 +0900

    test2

---
+++ week2/src/test/java/com/ll/ebooks/Week2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.ebooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
